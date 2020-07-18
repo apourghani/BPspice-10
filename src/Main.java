69155e4ef342fd400285905eb5f025c93398720a
apourghani <apourghani@gmail.com>
2020-07-18 12:06:32 +0430

    firstCommit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println("ihihihhi");
         GraphicalConsole graphicalConsole=new GraphicalConsole();
         graphicalConsole.run();
 
